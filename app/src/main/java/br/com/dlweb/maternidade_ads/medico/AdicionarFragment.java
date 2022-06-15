4dd70f88bed23e1cc81cb9454089618f1af40f52
Lucas Debatin <lucasdebatin@univali.br>
2022-06-14 21:06:50 -0300

    CM - finalização das telas do CRUD de médico e navegação entre fragmentos.

--- app/src/main/java/br/com/dlweb/maternidade_ads/medico/AdicionarFragment.java
+++ app/src/main/java/br/com/dlweb/maternidade_ads/medico/AdicionarFragment.java
@@ -11,9 +11,10 @@ import android.widget.Button;
 
 import br.com.dlweb.maternidade_ads.R;
 
-public class MainFragment extends Fragment {
+public class AdicionarFragment extends Fragment {
 
-    public MainFragment() {
+    public AdicionarFragment() {
+        // Required empty public constructor
     }
 
     @Override
@@ -24,28 +25,15 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.medico_fragment_adicionar, container, false);
 
-        View v = inflater.inflate(R.layout.medico_fragment_main, container, false);
-
-        if (savedInstanceState == null) {
-            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_medico, new ListarFragment()).commit();
-        }
         Button btnAdicionar = v.findViewById(R.id.button_adicionar_medico);
         btnAdicionar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_medico, new AdicionarFragment()).commit();
-            }
-        });
-
-        Button btnListar = v.findViewById(R.id.button_listar_medico);
-        btnListar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_medico, new ListarFragment()).commit();
             }
         });
-
         // Inflate the layout for this fragment
         return v;
     }
