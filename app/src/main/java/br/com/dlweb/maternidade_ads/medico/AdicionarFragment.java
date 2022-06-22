e76901c03c097ba24a2e687a616792483d3073f5
Lucas Debatin <lucasdebatin@univali.br>
2022-06-21 22:05:45 -0300

    CM - adicionado persistência de dados no CRUD do médico.

--- app/src/main/java/br/com/dlweb/maternidade_ads/medico/AdicionarFragment.java
+++ app/src/main/java/br/com/dlweb/maternidade_ads/medico/AdicionarFragment.java
@@ -8,10 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.dlweb.maternidade_ads.R;
+import br.com.dlweb.maternidade_ads.database.DatabaseHelper;
 
 public class AdicionarFragment extends Fragment {
+
+    private EditText etNome;
+    private EditText etEspecialidade;
+    private EditText etCelular;
 
     public AdicionarFragment() {
         // Required empty public constructor
@@ -27,14 +34,37 @@ public class AdicionarFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.medico_fragment_adicionar, container, false);
 
+        etNome = v.findViewById(R.id.editText_nome_medico);
+        etEspecialidade = v.findViewById(R.id.editText_especialidade_medico);
+        etCelular = v.findViewById(R.id.editText_celular_medico);
+
         Button btnAdicionar = v.findViewById(R.id.button_adicionar_medico);
         btnAdicionar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_medico, new ListarFragment()).commit();
+                adicionar();
             }
         });
         // Inflate the layout for this fragment
         return v;
+    }
+
+    private void adicionar() {
+        if (etNome.getText().toString().equals("")) {
+            Toast.makeText(getActivity(), "Por favor, informe o nome do médico", Toast.LENGTH_LONG).show();
+        } else if (etEspecialidade.getText().toString().equals("")) {
+            Toast.makeText(getActivity(), "Por favor, informe a especialidade do médico", Toast.LENGTH_LONG).show();
+        } else if (etCelular.getText().toString().equals("")) {
+            Toast.makeText(getActivity(), "Por favor, informe o número do celular do médico", Toast.LENGTH_LONG).show();
+        } else {
+            DatabaseHelper databaseHelper = new DatabaseHelper(getActivity());
+            Medico m = new Medico();
+            m.setNome(etNome.getText().toString());
+            m.setEspecialidade(etEspecialidade.getText().toString());
+            m.setCelular(etCelular.getText().toString());
+            databaseHelper.createMedico(m);
+            Toast.makeText(getActivity(), "Médico salvo", Toast.LENGTH_LONG).show();
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_medico, new ListarFragment()).commit();
+        }
     }
 }
