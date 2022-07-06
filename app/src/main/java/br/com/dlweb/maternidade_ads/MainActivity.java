b7b5b2d32bedc854479085c2fadc55f884d9d14c
Lucas Debatin <lucasdebatin@univali.br>
2022-07-05 21:22:35 -0300

    CM - notificação.

--- app/src/main/java/br/com/dlweb/maternidade_ads/MainActivity.java
+++ app/src/main/java/br/com/dlweb/maternidade_ads/MainActivity.java
@@ -1,12 +1,18 @@
 package br.com.dlweb.maternidade_ads;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 import br.com.dlweb.maternidade_ads.medico.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "123456";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, new MainFragment()).commit();
         }
+
+        Intent intent = new Intent(this, CameraActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Nova função no aplicativo")
+                .setContentText("Tire fotos pelo nosso aplicativo")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(1, builder.build());
     }
 }
